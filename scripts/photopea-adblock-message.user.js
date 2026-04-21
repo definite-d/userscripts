813793f6c62bc7adfa106d98b4d378a28668a2e1
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>
2026-04-21 04:30:03 +0100

    fix(photopea): add missing metadata for platform compatibility
    - Add license, homepageURL, supportURL
    - Add updateURL and downloadURL
    - Update namespace and bump version to 1.0.4

--- scripts/photopea-adblock-message.user.js
+++ scripts/photopea-adblock-message.user.js
@@ -1,9 +1,14 @@
 // ==UserScript==
 // @name         Remove Photopea Adblock Nag
-// @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @namespace    https://github.com/definite-d/userscripts
+// @version      1.0.4
 // @description  Removes the nag that says "Something is changing our code..."
 // @author       Afam-Ifediogor, U. Divine
+// @license      MIT
+// @homepageURL  https://github.com/definite-d/userscripts
+// @supportURL   https://github.com/definite-d/userscripts/issues
+// @updateURL    https://raw.githubusercontent.com/definite-d/userscripts/main/scripts/photopea-adblock-message.user.js
+// @downloadURL  https://raw.githubusercontent.com/definite-d/userscripts/main/scripts/photopea-adblock-message.user.js
 // @match        https://www.photopea.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=photopea.com
 // @grant        none
